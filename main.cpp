a6f77ac33b497b691c6fd38299fc2bfc8427b161
Kushal-Paudel <118868175+Kushal-Paudel@users.noreply.github.com>
2023-08-19 14:51:47 +0545

    Constellations added. Total-13

--- main.cpp
+++ main.cpp
@@ -173,8 +173,7 @@ int main()
 	vec.y = sin(glm::radians(latitude));
 	vec.z = sin(glm::radians(lon)) * cos(glm::radians(latitude));
 	vec = glm::normalize(vec);
-	cameraFront = vec;
-	cameraPos += cameraFront;
+	cameraPos += vec;
 
 	float cubevertices[] =
 	{
@@ -249,10 +248,15 @@ int main()
 		glm::vec3(-8.370f,-3.403f,99.591f), // Alnitak
 
 		//leo
-		glm::vec3(86.448f,20.740f,45.788f), // Regulus
-		glm::vec3(96.674f,25.156f,4.622f), // Denebola
-		glm::vec3(83.859f,33.956f,42.598f), // Algieba
-		glm::vec3(91.775f,35.086f,18.609f), // Zosma
+		glm::vec3(86.690f,20.735f,45.331f), // Regulus
+glm::vec3(96.732f,24.929f,4.636f), // Denebola
+glm::vec3(85.243f,33.942f,39.769f), // Algieba
+glm::vec3(91.964f,34.830f,18.154f), // Zosma
+glm::vec3(94.607f,26.373f,18.816f), // Chertan
+glm::vec3(84.611f,28.840f,44.825f), // Eta Leonis
+glm::vec3(82.652f,39.742f,39.864f), // Adhafera
+glm::vec3(75.891f,43.660f,48.316f), // Rasalas
+glm::vec3(76.630f,40.142f,50.164f), // Epsilon Leonis
 
 
 		//ursa major
@@ -270,8 +274,6 @@ glm::vec3(-51.089f,85.941f,2.037f), // Caph
 glm::vec3(-47.207f,87.290f,12.330f), // Gamma Cassiopeiae
 glm::vec3(-45.118f,86.980f,19.970f), // Ruchbah
 glm::vec3(-38.765f,89.625f,21.554f), // Segin
-glm::vec3(-45.741f,87.563f,15.505f), // Navi
-
 
 		//scorpius
 glm::vec3(9.150f,-60.321f,-79.232f), // Shaula
@@ -285,6 +287,7 @@ glm::vec3(34.485f,-44.510f,-82.641f), // Antares
 glm::vec3(46.923f,-33.857f,-81.560f), // Acrab
 glm::vec3(46.043f,-38.462f,-80.004f), // Dschubba
 glm::vec3(45.022f,-43.193f,-78.150f), // Pi Scorpii
+glm::vec3(3.729f,-64.457f,-76.363f), // Iota Scorpii
 		
 		//taurus
 glm::vec3(-34.389f,28.417f,89.498f), // Aldebaran
@@ -295,7 +298,7 @@ glm::vec3(-12.850f,47.881f,86.846f), // Elnath
 glm::vec3(-10.428f,36.055f,92.689f), // Zeta Tauri
 glm::vec3(-47.268f,21.659f,85.420f), // Lambda Tauri
 glm::vec3(-59.924f,15.731f,78.496f), // Omicron Tauri
-glm::vec3(-13.655f,36.069f,92.264f), // Epsilon Tauri
+glm::vec3(-35.761f,27.588f,89.219f), // Theta square Tauri
 glm::vec3(-36.048f,32.910f,87.278f), // Ain
 glm::vec3(-29.919f,39.096f,87.042f), // Tau Tauri
 		
@@ -326,6 +329,75 @@ glm::vec3(0.871f,99.823f,-5.890f), // Yildun
 glm::vec3(4.506f,99.019f,-13.229f), // Epsilon Ursae Minoris
 glm::vec3(11.816f,97.719f,-17.644f), // Zeta Ursae Minoris
 glm::vec3(10.847f,96.926f,-22.084f), // Eta Ursae Minoris
+
+//pegasus
+glm::vec3(-87.225f,48.803f,3.181f), // Alpheratz
+glm::vec3(-96.269f,26.309f,6.337f), // Algenib
+glm::vec3(-93.692f,26.304f,-23.019f), // Markab
+glm::vec3(-85.503f,47.228f,-21.417f), // Scheat
+glm::vec3(-92.235f,19.005f,-33.639f), // Homam
+glm::vec3(-88.438f,10.516f,-45.478f), // Biham
+glm::vec3(-81.978f,17.343f,-54.579f), // Enif
+glm::vec3(-81.735f,50.501f,-27.733f), // Matar
+glm::vec3(-74.271f,54.889f,-38.353f), // Pi Pegasi
+glm::vec3(-86.958f,40.172f,-28.716f), // Lambda Pegasi
+glm::vec3(-79.785f,42.978f,-42.277f), // Iota Pegasi
+glm::vec3(-75.002f,43.451f,-49.867f), // Kappa Pegasi
+
+//canis major
+glm::vec3(31.288f, -48.944f, 81.398f), // Aludra
+glm::vec3(33.206f, -44.555f, 83.140f), // Omega Canis Majoris
+glm::vec3(26.589f, -44.485f, 85.522f), // Wezen
+glm::vec3(22.144f, -48.438f, 84.637f), // Adhara
+glm::vec3(8.115f, -50.105f, 86.161f), // Furud
+glm::vec3(25.029f, -40.442f, 87.966f), // Omicron² Canis Majoris
+glm::vec3(18.736f, -28.763f, 93.924f), // Sirius
+glm::vec3(9.550f, -30.828f, 94.649f), // Mirzam
+glm::vec3(15.030f, -32.979f, 93.202f), // Nu² Canis Majoris
+glm::vec3(14.997f, -39.022f, 90.843f), // Xi² Canis Majoris
+glm::vec3(23.643f, -29.328f, 92.633f), // Iota Canis Majoris
+glm::vec3(26.512f, -26.977f, 92.571f), // Multiphen
+glm::vec3(23.233f, -20.882f, 94.996f), // Theta Canis Majoris
+
+//aquarius
+glm::vec3(-94.009f, 0.007f, -34.092f), // Eta Aquarii
+glm::vec3(-96.132f, -17.291f, -21.439f), // Psi1 Aquarii
+glm::vec3(-91.574f, -34.133f, -21.194f), // B1 Aquarii
+glm::vec3(-85.595f, -23.785f, -45.911f), // Iota Aquarii
+glm::vec3(-92.832f, -22.860f, -29.320f), // Tau Aquarii
+glm::vec3(-87.839f, -0.405f, -47.793f), // Sadalmelik
+glm::vec3(-79.603f, -9.508f, -59.774f), // Sadalsuud
+glm::vec3(-92.365f, -27.030f, -27.169f), // Skat
+glm::vec3(-89.129f, -13.326f, -43.341f), // Ancha
+glm::vec3(-67.296f, -16.503f, -72.103f), // Albali
+glm::vec3(-90.934f, -2.188f, -41.549f), // Sadachbia
+glm::vec3(-94.932f, -12.980f, -28.624f), // Lambda Aquarii
+glm::vec3(-92.764f, -0.178f, -37.347f), // Zeta Aquarii
+
+//sagittarius
+glm::vec3(-25.885f, -46.387f, -84.724f), // Tau Sagittarii
+glm::vec3(-21.421f, -44.260f, -87.076f), // Nunki
+glm::vec3(-23.394f, -49.755f, -83.529f), // Ascella
+glm::vec3(-18.275f, -45.343f, -87.235f), // Phi Sagittarii
+glm::vec3(-8.627f, -42.927f, -89.905f), // Kaus Borealis
+glm::vec3(-9.823f, -49.741f, -86.194f), // Kaus Media
+glm::vec3(-8.673f, -56.475f, -82.069f), // Kaus Australis
+glm::vec3(-6.606f, -59.841f, -79.846f), // Eta Sagittarii
+glm::vec3(-2.701f, -50.640f, -86.187f), // Alnasl (Gamma2 Sagittarii)
+
+//cygnus
+glm::vec3(-19.774f, 80.185f, -56.386f), // Kappa Cygni
+glm::vec3(-23.989f, 78.546f, -57.053f), // Iota Cygni
+glm::vec3(-31.230f, 70.936f, -63.188f), // Delta Cygni
+glm::vec3(-44.330f, 64.713f, -62.025f), // Sadr
+glm::vec3(-45.511f, 71.145f, -53.546f), // Deneb
+glm::vec3(-55.219f, 56.006f, -61.760f), // Epsilon Cygni
+glm::vec3(-65.191f, 50.487f, -56.580f), // Zeta Cygni
+glm::vec3(-72.899f, 48.289f, -48.517f), // Mu Cygni
+glm::vec3(-40.019f, 57.545f, -71.324f), // Eta Cygni
+glm::vec3(-34.059f, 46.962f, -81.453f), // Albireo
+
+//
 	};
 
 	float starvertices[] =
@@ -337,13 +409,192 @@ glm::vec3(10.847f,96.926f,-22.084f), // Eta Ursae Minoris
 		-12.187f, -0.524f, 99.253f,   // Mintaka
 		-10.364f, -2.094f, 99.439f,   // Alnilam
 		-5.267f, -16.797f, 98.438f,   // Saiph
-		-8.370f, -3.403f, 99.591f     // Alnitak //6
+		-8.370f, -3.403f, 99.591f,     // Alnitak //6
+
+		//leo
+		86.690f, 20.735f, 45.331f, // Regulus // 7
+96.732f, 24.929f, 4.636f, // Denebola // 8
+85.243f, 33.942f, 39.769f, // Algieba // 9
+91.964f, 34.830f, 18.154f, // Zosma // 10
+94.607f, 26.373f, 18.816f, // Chertan // 11
+84.611f, 28.840f, 44.825f, // Eta Leonis // 12
+82.652f, 39.742f, 39.864f, // Adhafera // 13
+75.891f, 43.660f, 48.316f, // Rasalas // 14
+76.630f, 40.142f, 50.164f, // Epsilon Leonis // 15
+
+//ursa major
+45.913f, 88.089f, 11.504f, // Dubhe // 16
+53.581f, 83.282f, 13.895f, // Merak // 17
+59.208f, 80.572f, 1.594f, // Phecda // 18
+54.297f, 83.896f, -3.662f, // Megrez // 19
+54.443f, 82.855f, -13.079f, // Alioth // 20
+53.664f, 81.835f, -20.573f, // Mizar // 21
+58.149f, 75.825f, -29.485f, // Alkaid // 22
+
+// 23 //cassiopeia
+-53.895f, 83.533f, 10.843f, // Schedar // 23
+-51.089f, 85.941f, 2.037f, // Caph // 24
+-47.207f, 87.290f, 12.330f, // Gamma Cassiopeiae // 25
+-45.118f, 86.980f, 19.970f, // Ruchbah // 26
+-38.765f, 89.625f, 21.554f, // Segin // 27
+
+// 28 //scorpius
+9.150f, -60.321f, -79.232f, // Shaula // 28
+5.440f, -63.013f, -77.458f, // Kappa Scorpii // 29
+6.517f, -69.441f, -71.663f, // Sargas // 30
+13.742f, -68.518f, -71.529f, // N Scorpii // 31
+20.566f, -67.430f, -70.924f, // Zeta² Scorpii // 32
+24.864f, -56.353f, -78.779f, // Epsilon Scorpii // 33
+31.218f, -47.286f, -82.398f, // Tau Scorpii // 34
+34.485f, -44.510f, -82.641f, // Antares // 35
+46.923f, -33.857f, -81.560f, // Acrab // 36
+46.043f, -38.462f, -80.004f, // Dschubba // 37
+45.022f, -43.193f, -78.150f, // Pi Scorpii // 38
+3.729f, -64.457f, -76.363f, // Iota Scorpii // 39
+
+// 40 //taurus
+-34.389f, 28.417f, 89.498f, // Aldebaran // 40
+-41.886f, 27.428f, 86.564f, // Gamma Tauri // 41
+-49.771f, 40.841f, 76.517f, // Pleiades // 42
+-38.084f, 30.141f, 87.413f, // Delta Tauri // 43
+-12.850f, 47.881f, 86.846f, // Elnath // 44
+-10.428f, 36.055f, 92.689f, // Zeta Tauri // 45
+-47.268f, 21.659f, 85.420f, // Lambda Tauri // 46
+-59.924f, 15.731f, 78.496f, // Omicron Tauri // 47
+-35.761f, 27.588f, 89.219f, // Theta square Tauri // 48
+-36.048f, 32.910f, 87.278f, // Ain // 49
+-29.919f, 39.096f, 87.042f, // Tau Tauri // 50
+
+
+// 51 //gemini
+39.162f, 46.993f, 79.107f, // Pollux // 51
+34.060f, 52.829f, 77.775f, // Castor // 52
+40.457f, 41.104f, 81.693f, // Kappa Gem // 53
+36.345f, 45.181f, 81.472f, // Upsilon Gem // 54
+32.889f, 46.561f, 82.161f, // Iota Gem // 55
+27.250f, 50.272f, 82.038f, // Tau Gem // 56
+20.715f, 55.861f, 80.314f, // Theta Gem // 57
+34.907f, 37.428f, 85.911f, // Wasat // 58
+32.305f, 28.368f, 90.286f, // Lambda Gem // 59
+25.970f, 35.042f, 89.987f, // Mekbuda // 60
+19.019f, 22.274f, 95.615f, // Xi Gem // 61
+15.709f, 28.234f, 94.636f, // Alhena // 62
+17.249f, 42.467f, 88.876f, // Mebsuta // 63
+14.069f, 34.382f, 92.844f, // Nu Gem // 64
+9.656f, 38.284f, 91.875f, // Tejat // 65
+5.746f, 38.284f, 92.202f, // Propus // 66
+3.806f, 39.547f, 91.769f, // 1 Geminorum // 67
+
+// 68 //ursa minor
+-0.795f, 99.994f, 0.802f, // Polaris // 68
+20.073f, 96.201f, -18.508f, // Kochab // 69
+19.963f, 95.016f, -23.948f, // Pherkad // 70
+0.871f, 99.823f, -5.890f, // Yildun // 71
+4.506f, 99.019f, -13.229f, // Epsilon Ursae Minoris // 72
+11.816f, 97.719f, -17.644f, // Zeta Ursae Minoris // 73
+10.847f, 96.926f, -22.084f, // Eta Ursae Minoris // 74
+
+// 75 //pegasus
+- 87.225f, 48.803f, 3.181f, // Alpheratz // 75
+-96.269f, 26.309f, 6.337f, // Algenib // 76
+-93.692f, 26.304f, -23.019f, // Markab // 77
+-85.503f, 47.228f, -21.417f, // Scheat // 78
+-92.235f, 19.005f, -33.639f, // Homam // 79
+-88.438f, 10.516f, -45.478f, // Biham // 80
+-81.978f, 17.343f, -54.579f, // Enif // 81
+-81.735f, 50.501f, -27.733f, // Matar // 82
+-74.271f, 54.889f, -38.353f, // Pi Pegasi // 83
+-86.958f, 40.172f, -28.716f, // Lambda Pegasi // 84
+-79.785f, 42.978f, -42.277f, // Iota Pegasi // 85
+-75.002f, 43.451f, -49.867f, // Kappa Pegasi // 86
+
+// 87 //canis major
+31.288f, -48.944f, 81.398f, // Aludra // 87
+33.206f, -44.555f, 83.140f, // Omega Canis Majoris // 88
+26.589f, -44.485f, 85.522f, // Wezen // 89
+22.144f, -48.438f, 84.637f, // Adhara // 90
+8.115f, -50.105f, 86.161f, // Furud // 91
+25.029f, -40.442f, 87.966f, // Omicron² Canis Majoris // 92
+18.736f, -28.763f, 93.924f, // Sirius // 93
+9.550f, -30.828f, 94.649f, // Mirzam // 94
+15.030f, -32.979f, 93.202f, // Nu² Canis Majoris // 95
+14.997f, -39.022f, 90.843f, // Xi² Canis Majoris // 96
+23.643f, -29.328f, 92.633f, // Iota Canis Majoris // 97
+26.512f, -26.977f, 92.571f, // Multiphen // 98
+23.233f, -20.882f, 94.996f, // Theta Canis Majoris // 99
+
+// 100 //aquarius
+-94.009f, 0.007f, -34.092f, // Eta Aquarii // 100
+-96.132f, -17.291f, -21.439f, // Psi1 Aquarii // 101
+-91.574f, -34.133f, -21.194f, // B1 Aquarii // 102
+-85.595f, -23.785f, -45.911f, // Iota Aquarii // 103
+-92.832f, -22.860f, -29.320f, // Tau Aquarii // 104
+-87.839f, -0.405f, -47.793f, // Sadalmelik // 105
+-79.603f, -9.508f, -59.774f, // Sadalsuud // 106
+-92.365f, -27.030f, -27.169f, // Skat // 107
+-89.129f, -13.326f, -43.341f, // Ancha // 108
+-67.296f, -16.503f, -72.103f, // Albali // 109
+-90.934f, -2.188f, -41.549f, // Sadachbia // 110
+-94.932f, -12.980f, -28.624f, // Lambda Aquarii // 111
+-92.764f, -0.178f, -37.347f, // Zeta Aquarii // 112
+
+// 113 //sagittarius
+-25.885f, -46.387f, -84.724f, // Tau Sagittarii // 113
+-21.421f, -44.260f, -87.076f, // Nunki // 114
+-23.394f, -49.755f, -83.529f, // Ascella // 115
+-18.275f, -45.343f, -87.235f, // Phi Sagittarii // 116
+-8.627f, -42.927f, -89.905f, // Kaus Borealis // 117
+-9.823f, -49.741f, -86.194f, // Kaus Media // 118
+-8.673f, -56.475f, -82.069f, // Kaus Australis // 119
+-6.606f, -59.841f, -79.846f, // Eta Sagittarii // 120
+-2.701f, -50.640f, -86.187f, // Alnasl (Gamma2 Sagittarii) // 121
+
+// 122 //cygnus
+- 19.774f, 80.185f, -56.386f, // Kappa Cygni // 122
+-23.989f, 78.546f, -57.053f, // Iota Cygni // 123
+-31.230f, 70.936f, -63.188f, // Delta Cygni // 124
+-44.330f, 64.713f, -62.025f, // Sadr // 125
+-45.511f, 71.145f, -53.546f, // Deneb // 126
+-55.219f, 56.006f, -61.760f, // Epsilon Cygni // 127
+-65.191f, 50.487f, -56.580f, // Zeta Cygni // 128
+-72.899f, 48.289f, -48.517f, // Mu Cygni // 129
+-40.019f, 57.545f, -71.324f, // Eta Cygni // 130
+-34.059f, 46.962f, -81.453f, // Albireo // 131
+
+
+
+
 	};
 
 	const unsigned int indices[]
 	{
 		//orion
-		0,2,0,6,6,4,4,3,3,1,6,5,3,2,1,5
+		0,2,0,6,6,4,4,3,3,1,6,5,3,2,1,5,
+		//leo
+		8,11,8,10,11,7,11,10,10,9,7,12,12,9,9,13,13,14,14,15,
+		//ursa major
+		22,21,21,20,20,19,19,18,19,16,18,17,17,16,
+		//cassiopeia
+		27,26,26,25,25,23,23,24,
+		//scorpius
+		28,29,29,39,39,30,30,31,31,32,32,33,33,34,34,35,35,36,35,37,35,38,
+		//taurus
+		45,40,44,50,50,49,49,40,49,43,43,41,43,42,41,46,46,47,40,48,48,41,
+		//gemini
+		51,54,54,53,54,58,54,55,58,59,59,61,58,60,60,62,55,56,56,57,56,63,63,64,63,65,65,66,66,67,52,56,
+		//ursa minor
+		68,71,71,72,72,73,73,69,73,74,69,70,70,74,
+		//pegasus
+		75,76,75,78,76,77,77,78,77,79,79,80,80,81,78,84,78,82,84,85,85,86,82,83,
+		//canis major
+		87,88,88,89,89,90,89,92,90,91,92,93,93,94,93,95,93,97,95,96,97,98,97,99,98,99,
+		//aquarius
+		109,106,106,105,105,110,110,112,112,100,100,111,111,101,101,102,105,108,108,104,104,107,108,103,
+		//sagittarius
+		113,114,113,115,114,116,115,119,119,120,119,118,118,116,118,121,121,119,118,117,117,116,115,116,
+		//cygnus
+		122,123,123,124,124,125,125,126,125,127,127,128,128,129,125,130,130,131,
+
 	};
 
 
@@ -499,7 +750,7 @@ glm::vec3(10.847f,96.926f,-22.084f), // Eta Ursae Minoris
 	bool firstframe=true;
 
 	//related to showing surface
-	bool showSurface=false;
+	bool showSurface=true;
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -536,16 +787,17 @@ glm::vec3(10.847f,96.926f,-22.084f), // Eta Ursae Minoris
 
 		//}
 
-		//position camera above the surface
-		if (showSurface)
+		//position camera above the surface 
+		//restricts camera movement
+		/*if (showSurface)
 		{
 			glm::vec3 vec;
 			vec.x = cos(glm::radians(lon)) * cos(glm::radians(latitude));
 			vec.y = sin(glm::radians(latitude));
 			vec.z = sin(glm::radians(lon)) * cos(glm::radians(latitude));
 			vec = glm::normalize(vec);
-			cameraPos = vec*glm::vec1(3.0f);
-		}
+			cameraPos = vec*glm::vec1(1.0f);
+		}*/
 
 		//stars
 		shaderprogram.Activate();
